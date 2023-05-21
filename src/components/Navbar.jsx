8d8fa22c33ab6e08fd8c8d708045d4267263797b
Marlon Couto <marloncouto199@gmail.com>
2023-05-21 15:00:53 -0300

    feat: cria componente Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,37 @@
+import { useContext } from 'react';
+
+import Context from '../context/Context';
+import CloseIcon from './svg/CloseIcon';
+import HamburgerIcon from './svg/HamburgerIcon';
+import MoonIcon from './svg/MoonIcon';
+import SunIcon from './svg/SunIcon';
+
 export default function Navbar() {
+  const { setIsOpen, isOpen } = useContext(Context);
+
   return (
     <div className="navbar bg-base-100">
       <div className="flex-none">
-        <button className="btn btn-square btn-ghost">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+        <button className="btn btn-circle btn-ghost">
+          <label className="swap swap-rotate">
+            {/* <!-- this hidden checkbox controls the state --> */}
+            <input type="checkbox" onChange={() => setIsOpen(!isOpen)} checked={isOpen} />
+            <HamburgerIcon />
+            <CloseIcon />
+          </label>
         </button>
       </div>
       <div className="flex-1">
         <a className="btn btn-ghost normal-case text-lg" href='https://github.com/marloncoutodev'>Marlon Couto</a>
       </div>
       <div className="flex-none">
-        <button className="btn btn-square btn-ghost">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" className="inline-block w-5 h-5 stroke-current fill-current">{/* <!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --> */}<path d="M223.5 32C100 32 0 132.3 0 256S100 480 223.5 480c60.6 0 115.5-24.2 155.8-63.4c5-4.9 6.3-12.5 3.1-18.7s-10.1-9.7-17-8.5c-9.8 1.7-19.8 2.6-30.1 2.6c-96.9 0-175.5-78.8-175.5-176c0-65.8 36-123.1 89.3-153.3c6.1-3.5 9.2-10.5 7.7-17.3s-7.3-11.9-14.3-12.5c-6.3-.5-12.6-.8-19-.8z" /></svg>
+        <button className="btn btn-circle btn-ghost">
+          <label className="swap swap-rotate">
+            {/* <!-- this hidden checkbox controls the state --> */}
+            <input type="checkbox" />
+            <SunIcon />
+            <MoonIcon />
+          </label>
         </button>
       </div>
     </div>
