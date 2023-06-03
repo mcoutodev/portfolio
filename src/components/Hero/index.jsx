3c65771b447303deb886cd2d9fa2a532e4accba9
Marlon Couto <marloncouto@pop-os.localdomain>
2023-06-03 19:33:04 -0300

    feat: atualiza Hero.jsx e cria Hero.module.scss

---
+++ src/components/Hero/index.jsx
@@ -0,0 +1,25 @@
+import Avatar from '../Avatar';
+import styles from './Hero.module.scss';
+
+export default function Hero() {
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <Avatar />
+        <div>
+          <div className={styles.animation}>
+            <span className={`${styles.first} text-primary`}>Hello,</span>
+            <span className={styles.slide}>
+              <span className={styles.second}>World!</span>
+            </span>
+          </div>
+          <p className="py-6">
+            Eu sou um desenvolvedor web full-stack com experiência em TypeScript, React, Tailwind e Node.js.
+            Bem-vindo ao meu portfólio pessoal, a casa é sua! 👻
+          </p>
+          <button className="btn btn-primary">Começar</button>
+        </div>
+      </div>
+    </div>
+  );
+}
