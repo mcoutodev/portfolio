76fca07db77f2628c10cc07644692553207ca2d9
Marlon Couto <marloncouto@pop-os.localdomain>
2023-06-03 19:32:26 -0300

    feat: atualiza Drawer.jsx

--- src/components/Drawer.jsx
+++ src/components/Drawer.jsx
@@ -4,21 +4,24 @@ import { useContext } from 'react';
 import Context from '../context/Context';
 
 export default function Drawer({ children }) {
-  const { isOpen, setIsOpen } = useContext(Context);
+  const { isOpen, setIsOpen, sections } = useContext(Context);
 
   return (
     <div className="drawer drawer-end">
       <input id="my-drawer" type="checkbox" className="drawer-toggle" checked={isOpen} onChange={() => setIsOpen(false)} />
       <div className="drawer-content">
-        {/* <!-- Page content here --> */}
+        {/* Page content here */}
         {children}
       </div>
       <div className="drawer-side">
         <label htmlFor="my-drawer" className="drawer-overlay"></label>
         <ul className="menu p-4 w-80 bg-base-100 text-base-content">
-          {/* <!-- Sidebar content here --> */}
-          <li><a>Sidebar Item 1</a></li>
-          <li><a>Sidebar Item 2</a></li>
+          {/* Sidebar content here */}
+          {sections.map(({ name, path }) => (
+            <li key={name}>
+              <a href={path}>{name}</a>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
