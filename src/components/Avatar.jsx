317ca2284dbd12bfd30f1f04abc4bb19354cc9cc
Marlon Couto <marloncouto@pop-os.localdomain>
2023-06-03 19:31:58 -0300

    feat: cria Avatar.jsx

---
+++ src/components/Avatar.jsx
@@ -0,0 +1,9 @@
+export default function Avatar() {
+  return (
+    <div className="avatar py-8">
+      <div className="w-40 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+        <img src="images/profile_verde.jpg" />
+      </div>
+    </div>
+  );
+}
