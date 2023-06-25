1872bb3cfaffc0cbdbb0fdf0c5d53df4f44346ba
Marlon Couto <marloncouto199@gmail.com>
2023-06-25 13:45:58 -0300

    feat: atualiza arquivos do ESLint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,24 @@
 module.exports = {
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
-  },
-}
+    env: { browser: true, es2020: true },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:import/recommended',
+        'plugin:import/typescript',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+    plugins: ['react-refresh'],
+    rules: {
+        'react-refresh/only-export-components': 'warn',
+        'import/order': [
+            'warn',
+            {
+                alphabetize: { order: 'asc' },
+                groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+                'newlines-between': 'always',
+            },
+        ],
+    },
+};
