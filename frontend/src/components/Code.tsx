f440b81a4ab6b1793ff704055e81331c620d099c
Marlon Couto <marloncouto199@gmail.com>
2023-07-05 22:19:21 -0300

    feat: atualiza o Code.tsx

--- frontend/src/components/Code.tsx
+++ frontend/src/components/Code.tsx
@@ -6,30 +6,35 @@ export const Code: FC = () => {
             <pre data-prefix="$">
                 <code>cat marlon_couto_dev.txt</code>
             </pre>
+            
             <pre
                 data-prefix=">"
                 className="text-success"
             >
                 <code>Nome: Marlon Couto</code>
             </pre>
+
             <pre
                 data-prefix=">"
                 className="text-success"
             >
                 <code>Nascimento: 17/01/1991</code>
             </pre>
+
             <pre
                 data-prefix=">"
                 className="text-success"
             >
                 <code>Nacionalidade: Brasileiro</code>
             </pre>
+
             <pre
                 data-prefix=">"
                 className="text-success"
             >
                 <code>Profissão: Desenvolvedor web</code>
             </pre>
+
             <pre data-prefix=">">
                 <code>END</code>
             </pre>
