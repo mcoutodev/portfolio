60de08147d6a7862321aae4e72101f6a43058b52
Marlon Couto <marloncouto199@gmail.com>
2023-07-08 15:54:26 -0300

    feat: atualiza layout dos componentes para telas maiores

--- frontend/src/components/Avatar.tsx
+++ frontend/src/components/Avatar.tsx
@@ -3,7 +3,7 @@ import { FC } from 'react';
 export const Avatar: FC = () => {
     return (
         <div className="avatar py-8">
-            <div className="w-40 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+            <div className="w-40 md:w-56 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
                 <img
                     src="images/profile_verde.jpg"
                     alt="Uma foto de um rapaz sorrindo, com árvores ao fundo."
