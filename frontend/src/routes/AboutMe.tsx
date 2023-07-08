0dbcc38ba346fc666a2cd0739c304d6f2d7b9795
Marlon Couto <marloncouto199@gmail.com>
2023-07-08 15:52:51 -0300

    feat: atualiza layout para telas maiores

--- frontend/src/routes/AboutMe.tsx
+++ frontend/src/routes/AboutMe.tsx
@@ -4,10 +4,14 @@ import { Avatar, Code, Description } from '../components';
 
 export const AboutMe: FC = () => {
     return (
-        <main className="flex flex-col items-center">
-            <Avatar />
-            <Code />
-            <Description />
+        <main className="flex justify-center pt-4">
+            <div className="flex flex-col items-center md:flex-row md:items-start max-w-7xl">
+                <div className="flex flex-col items-center md:mx-12 md:mt-8">
+                    <Avatar />
+                    <Code />
+                </div>
+                <Description />
+            </div>
         </main>
     );
 };
