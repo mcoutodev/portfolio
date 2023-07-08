b84d00d2ff632ec29b0bf4cc3cfa5cbff7f6190a
Marlon Couto <marloncouto199@gmail.com>
2023-07-08 15:54:00 -0300

    feat: cria SVG das skills

---
+++ frontend/src/components/svg/skills/index.ts
@@ -0,0 +1,31 @@
+import { CSSIcon } from './CSSIcon';
+import { DockerIcon } from './DockerIcon';
+import { GitIcon } from './GitIcon';
+import { HTMLIcon } from './HTMLIcon';
+import { JavaIcon } from './JavaIcon';
+import { LinuxIcon } from './LinuxIcon';
+import { MongoIcon } from './MongoIcon';
+import { MySQLIcon } from './MySQLIcon';
+import { NextIcon } from './NextIcon';
+import { NodeIcon } from './NodeIcon';
+import { PHPIcon } from './PHPIcon';
+import { PythonIcon } from './PythonIcon';
+import { ReactIcon } from './ReactIcon';
+import { TypeScriptIcon } from './TypeScriptIcon';
+
+export {
+    DockerIcon,
+    GitIcon,
+    HTMLIcon,
+    JavaIcon,
+    CSSIcon,
+    TypeScriptIcon,
+    LinuxIcon,
+    NodeIcon,
+    ReactIcon,
+    MongoIcon,
+    MySQLIcon,
+    PythonIcon,
+    PHPIcon,
+    NextIcon,
+};
